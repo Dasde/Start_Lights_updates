9e607e360f51a04a96a2dca1364aff5fa18d8c19
DaZD <dasde@free.fr>
2025-09-06 01:44:37 +0200

    Update test_script.lua

--- test_script.lua
+++ test_script.lua
@@ -1079,7 +1079,7 @@ if SERVER_MODE then
     slMgr.init(AppSettings.classicLightsScale, AppSettings.useSound, AppSettings.classicLightsOrientation,
     AppSettings.lightsModType, AppSettings.sendChatMessage, AppSettings.use3DLights, SERVER_MODE)
   end,5)
-  --return
+  return
 else
   slMgr.init(AppSettings.classicLightsScale, AppSettings.useSound, AppSettings.classicLightsOrientation,
   AppSettings.lightsModType, AppSettings.sendChatMessage, AppSettings.use3DLights, SERVER_MODE)
